3b75552bbeefaca0d5e096122416e8d8a5ae8489
Eric Laursen <ericjlaursen@gmail.com>
2019-08-30 06:37:29 -0400

    note sabout the process

--- src/routes/SelectableArea/index.js
+++ src/routes/SelectableArea/index.js
@@ -11,3 +11,50 @@ const SelectableArea = () => {
 }
 
 export default SelectableArea
+
+/*
+	Notes
+
+ 	- UPDATES state
+ 	- only responds to initial state && itself,
+ 		no other inputs besided user mouse/click
+	
+	
+
+	ELEMENTS
+		svg
+		gWrapper
+		
+	DATA
+		xScale
+		yScale
+
+	xAxisG
+		trans 0, height
+
+	areaPath
+		fill #ccc
+
+	brushWindow
+		gObj.append(g)
+		class brushGWindow
+		.call brushFn
+
+	brushFn
+		let brushFn = d3.brushX()
+			.handleSize(10)
+			.extent([ [0,0], [state.timeline.w, state.timeline.h] ])
+			.on('brush', brushedFn)
+
+	brushedFn
+		var selectedPixels = d3.event.selection || timeline.x.range();
+    var newValues = selectedPixels.map(state.timeline.xScale.invert)
+
+    updateLine(state.filteredData[state.activeCoin], newValues)
+
+
+	UPDATING TIMELINE
+		a fn passing data && yVariable?!
+		it updates other things on the screen, for later...
+
+*/
