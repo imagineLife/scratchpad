05c86d28c4bafdda9e9d6c6ebde1ba4b5a597eaf
Eric Laursen <ericjlaursen@gmail.com>
2019-08-31 14:34:59 -0400

    outline

--- src/routes/TextAndSelection/index.js
+++ src/routes/TextAndSelection/index.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
 const TextAndSelection = () => {
+	
+	let [srcText, setSrcText] = React.useState(null);
+
+	//load the text from textFile?!
+	React.useEffect(() => {
+		fetch('../../data/demo.txt')
+			.then(res => res.text())
+			.then(setSrcText)
+	}, [])
+
+	if(!srcText){
+		return(<p>loading...</p>)
+	}
 	return(
-		<h2>Text And Selection</h2>
+		<React.Fragment>
+			<h2>Text And Selection</h2>
+		</React.Fragment>
 	)
 }
 
