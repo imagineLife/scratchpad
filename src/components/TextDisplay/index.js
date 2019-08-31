01ae856df4ef22e375ed9cd8f4b113e8b9f1ddc2
Eric Laursen <ericjlaursen@gmail.com>
2019-08-31 15:38:34 -0400

    includes getHighlighted text fn

--- src/components/TextDisplay/index.js
+++ src/components/TextDisplay/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import './index.css'
 
-const TextDisplay = ({txt}) => {
+const getHighlightedText = (text, higlight) => {
+    // Split on higlight term and include term into parts, ignore case
+    let parts = text.split(new RegExp(`(${higlight})`, 'gi'));
+    return <span> { parts.map((part, i) => 
+        <span key={i} style={part.toLowerCase() === higlight.toLowerCase() ? { fontWeight: 'bold', fontSize: '22px' } : {} }>
+            { part }
+        </span>)
+    } </span>;
+}
+
+const TextDisplay = ({txt ,selectedWord}) => {
+
+	let resText = (selectedWord) ? getHighlightedText(txt, selectedWord) : txt;
+
 	return(
-		<p>{txt}</p>
+		<div id="text-display">{resText}</div>
 	)
 }
 
