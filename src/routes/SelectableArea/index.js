6259d02b46d886ba3f4c5ff6f004d082672097b2
Eric Laursen <ericjlaursen@gmail.com>
2019-09-01 11:25:33 -0400

    translateScale && commented logs

--- src/routes/SelectableArea/index.js
+++ src/routes/SelectableArea/index.js
@@ -4,22 +4,28 @@ import * as d3Arr from 'd3-array'
 import * as d3Shape from 'd3-shape'
 import * as d3Select from 'd3-selection'
 import * as brush from 'd3-brush'
+import TextDisplay from '../../components/TextDisplay'
 
 const SelectableArea = () => {
 	let [srcData, setSrcData] = React.useState(null)
 	let [brushFn, setBrushFn] = React.useState(null)
+	let [fullText, setFullText] = React.useState(null)
+	let [hoverArr, setHoverArr] = React.useState([0,175])
 	let brushRef = React.useRef()
 	
 	function brushedFn(){
 		var selectedPixels = d3Select.event.selection; //|| timeline.x.range()
-		console.log('selectedPixels')
-		console.log(selectedPixels)
+		setHoverArr(selectedPixels)
 	}
 	//load the data
 	React.useEffect(() => {
 		fetch('../../data/areaData.json')
 			.then(res => res.json())
 			.then(setSrcData)
+
+		fetch('../../data/fullText.txt')
+			.then(res => res.text())
+			.then(setFullText)
 	}, [])
 	
 	//connect the brush to the g wrapper?!
@@ -32,32 +38,33 @@ const SelectableArea = () => {
 			//build the brushFn
 			let brushFn = brush.brushX()
 				.handleSize(10)
-				.on('brush', () => brushedFn())
+				.on('brush', brushedFn)
 
 			// set the brushFn to the burshBox, 'instantiating'
 			// the brush UI element(s)
 			brushBox.call(brushFn);
 
 			//set the initial overlay to 1/4 width
-			/*
-				How Do I get this to ANIMATE 'onLoad'
-			*/
-			brushFn.move(brushBox, [0,175])
+			brushFn.move(brushBox, hoverArr)
 		}
-	}, [srcData])
+	}, [fullText])
 
-	if(!srcData){
+	if(!srcData || !fullText){
 		return (<p>Loading data...</p>)
 	}
 	
 	//Set Scales
 	let xScale = scale.scaleLinear()
-		.domain([1, srcData.length])
+		.domain([0, srcData.length - 1])
 		.range([0, 700])
 
 	let yScale = scale.scaleLinear()
 		.domain([0, d3Arr.max(srcData, d => d.y)])
 		.range([100, 0])
+
+	let translateScale = scale.scaleLinear()
+		.domain([0, 700])
+		.range([0, srcData.length - 1])
 
 	//build areaFn
 	let areaFn = d3Shape.area()
@@ -65,19 +72,44 @@ const SelectableArea = () => {
 		.y0(100)
 		.y1((d) => yScale(d.y))
 
+
+	// console.log('%c SCALED VALUES', 'background-color: orange; color: white;')
+	// console.log('srcData.length')
+	// console.log(srcData.length)
+	
+	// console.log('hoverArr')
+	// console.log(hoverArr)
+	
+	// console.log('translateScale(hoverArr[0])')
+	// console.log()
+	// console.log('translateScale(hoverArr[1])')		
+	// console.log(translateScale(hoverArr[1]))
+	// console.log('%c  - - - - ', 'background-color: orange; color: white;')
+	
+	// let resText = srcData.map((d, i) => {
+	// 	if(i >= translateScale(hoverArr[0]) && i <= translateScale(hoverArr[1])){
+	// 		return d
+	// 	}else return
+	// }).filter(d => d)
+	// console.log('resText')
+	// console.log(resText)
+	
 	return(
-		<svg id="selectable" style={{
-			width: '700px',
-			height: '100px',
-			border: '1px solid green'
-		}}>
-			<g className="g-wrapper">
-				<path d={areaFn(srcData)}
-					fill={'#ccc'}
-				/>
-				<g className="brush-g-window" ref={brushRef}></g>
-			</g>
-		</svg>
+		<React.Fragment>
+			<svg id="selectable" style={{
+				width: '700px',
+				height: '100px',
+				border: '1px solid green'
+			}}>
+				<g className="g-wrapper">
+					<path d={areaFn(srcData)}
+						fill={'#ccc'}
+					/>
+					<g className="brush-g-window" ref={brushRef}></g>
+				</g>
+			</svg>
+			<TextDisplay />
+		</React.Fragment>
 	)
 }
 
