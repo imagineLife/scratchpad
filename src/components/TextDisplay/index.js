409e0624902ee62944f67307815bd199728c03e6
Eric Laursen <ericjlaursen@gmail.com>
2019-09-08 12:48:27 -0400

    removes comment

--- src/components/TextDisplay/index.js
+++ src/components/TextDisplay/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './index.css'
 
-const THIS_UPPER_CASE_MESS = require('thatMess') //'thisUpperCaseMess'
-
 const getQueriedWord = (text, hlText) => {
 
     //remove existing tags associated with this selection
@@ -29,59 +27,10 @@ const TextDisplay = ({txt ,commonWord}) => {
 	let resText = (commonWord) ? getQueriedWord(txt, commonWord, themeType) : txt;
     // let resText = (commonWord) ? getLongestWord(txt, longestWord) : txt;
 
+    resText = !resText ? 'placeholder...' : resText;
 	return(
 		<div id="text-display">{resText}</div>
 	)
 }
 
 export default TextDisplay;
-
-
-/*
-
-    TextDisplay = () => {
-        
-        // { srcText } = useTextStore()
-        { longestWord, theme, commonWord } = useStoreContext()
-        
-        const setLongestWord = txtParam => {
-            return txtParam.applyLongestWordRegexHere...()
-        }
-
-        NOTE: 
-            helper function will prob be in a separate file
-
-        let alteredText = srcText
-        alteredText = longestWord && setLongestWord(alteredText)
-        alteredText = theme && setTheme(alteredText)
-        alteredText = commonWord && setCommonWord(alteredText)
-
-        return alteredText
-
-    }
-
-    resText = txt; 
-    selectedWord && getQueriedWord
-
-
-    Use Context
-    Parent Context
-        textBlob
-
-    longestWordCtx
-        longWordVal setter
-
-    themeCtx
-        theme, setTheme
-
-    <TextDisplay>
-        let { txtCtn} = useContext(thisContext)
-        let { lwCtx } = useContext(LongestWordCtx)
-        let { cwCtx } = useContext(thisContext)
-        show fancy text here
-    </TextDisplay>
-
-    <LongestWordProvider>
-        <LongestWord />
-    </LongestWordProvider>        
-*
