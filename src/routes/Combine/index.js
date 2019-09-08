5c0cb853fef81559fbae7553ed6c0e3f38658efe
Eric Laursen <ericjlaursen@gmail.com>
2019-09-08 14:00:06 -0400

    re-orders Return contents

--- src/routes/Combine/index.js
+++ src/routes/Combine/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import WordList from '../../components/WordList'
 import WrappedTextDisplay from '../../components/TextDisplay/TextContextWrapper'
+import WrappedWordList from '../../components/WordList/ContextWrapper'
 const Combined = () => {
 	
 	return(
-		<WrappedTextDisplay />
+		<React.Fragment>
+			<WrappedWordList />
+			<WrappedTextDisplay />
+		</React.Fragment>
 	)
 }
 
