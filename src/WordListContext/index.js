0ab5d8ad43549ae7488e68b6778bd1290116f8b8
Eric Laursen <ericjlaursen@gmail.com>
2019-09-09 06:27:00 -0400

    logs

--- src/WordListContext/index.js
+++ src/WordListContext/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const WordListContext = React.createContext(); 
-const {Provider, Consumer} = WordListContext
+const {Provider, Consumer} = WordListContext;
 
 const WordListProvider = (props) => {
 
@@ -9,7 +9,7 @@ const WordListProvider = (props) => {
 	let [selectedWord, setSelectedWord] = React.useState(null);
 	let [commonWords] = React.useState(['president', 'the', 'we', 'to'])
 	let [listData] = React.useState({
-		"Common Words": ["our", "we", "the", "them"],
+		"Common Words": ['president', 'the', 'we', 'to'],
 		"Longest Words": [
 			"Administration",
 			"Infrastructure",
@@ -38,13 +38,33 @@ const WordListProvider = (props) => {
 	}, [])
 
 
-	return(<Provider value={{
-		listData,
-		selectedWord,
-		setSelectedWord,
-		setSelectedList,
-		commonWords
-	}}>
+	console.log('%c WORD LIST CONTEXT', 'background-color: orange; color: white;')
+	
+	/*
+		es6, commented for sanity-purpose, 
+		debigging the non=updating-text-blob
+
+		let contextVal = {
+			listData,
+			selectedWord,
+			setSelectedWord,
+			setSelectedList,
+			commonWords
+		}
+	*/
+
+	let contextVal = {
+		listData:listData,
+		selectedWord:selectedWord,
+		setSelectedWord:setSelectedWord,
+		setSelectedList:setSelectedList,
+		commonWords:commonWords
+	}
+
+	console.log('contextVal.selectedWord')
+	console.log(contextVal.selectedWord)	
+	
+	return(<Provider value={contextVal}>
 		{props.children}
 	</Provider>)
 }
