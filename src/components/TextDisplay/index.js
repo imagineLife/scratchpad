fa26ff9e7176155ee469fec340594e724da91348
Eric Laursen <ericjlaursen@gmail.com>
2019-09-18 06:10:21 -0400

    comments out text-parsing-logic

--- src/components/TextDisplay/index.js
+++ src/components/TextDisplay/index.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import './index.css'
 
-const getQueriedWord = (text, hlText) => {
+// const getQueriedWord = (text, hlText) => {
 
-    //remove existing tags associated with this selection
-    let removeRegex = /.\w*\s\w*=\"selected-text\".(\w*)<\/\w*>/g;
+//     //remove existing tags associated with this selection
+//     let removeRegex = /.\w*\s\w*=\"selected-text\".(\w*)<\/\w*>/g;
     
-    /*
-        let newStyleRegex = new RegExp(`\W(${hlText})\W`, 'gi');
-        IN JS th '\' has to be escaped, a-la \\b 
-    */ 
+//     /*
+//         let newStyleRegex = new RegExp(`\W(${hlText})\W`, 'gi');
+//         IN JS th '\' has to be escaped, a-la \\b 
+//     */ 
     
-    // add tags associated with this selection
-    let newStyleRegex = new RegExp(`\\b(${hlText})\\b`, 'gi');
+//     // add tags associated with this selection
+//     let newStyleRegex = new RegExp(`\\b(${hlText})\\b`, 'gi');
     
-    //update the text body
-    let resText = text.replace(removeRegex,"$1").replace(newStyleRegex, `<span class="selected-text">$1</span>`)
+//     //update the text body
+//     let resText = text.replace(removeRegex,"$1").replace(newStyleRegex, `<span class="selected-text">$1</span>`)
 
     
-    return <p dangerouslySetInnerHTML={{__html: resText}}></p>
+//     return <p dangerouslySetInnerHTML={{__html: resText}}></p>
 
-}
+// }
 
 const TextDisplay = (props) => {
 
@@ -28,12 +28,12 @@ const TextDisplay = (props) => {
     console.log(props)
     
 
-	let resText = (props.commonWord) ? getQueriedWord(props.text, props.commonWord) : props.text;
+	// let resText = (props.commonWord) ? getQueriedWord(props.text, props.commonWord) : props.text;
     // let resText = (commonWord) ? getLongestWord(txt, longestWord) : txt;
 
-    resText = !resText ? 'placeholder...' : resText;
+    // resText = !resText ? 'placeholder...' : resText;
 	return(
-		<div id="text-display">{resText}</div>
+		<div id="text-display">{props.text || 'placeholder...'}</div>
 	)
 }
 
