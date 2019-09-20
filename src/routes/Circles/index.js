f337f128a1e5112b30276ae24cd0b98ee5422523
Eric Laursen <ericjlaursen@gmail.com>
2019-09-20 05:26:23 -0400

    renders box

--- src/routes/Circles/index.js
+++ src/routes/Circles/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import './index.css'
 const Circles = () => {
 	
 	return(
 		<React.Fragment>
 			<h2>Circles</h2>
+			<div id="circlesBox"></div>
 		</React.Fragment>
 	)
 }
