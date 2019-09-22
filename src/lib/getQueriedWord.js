8596c617f93de7895f21a66b339a5c265e72d158
Eric Laursen <ericjlaursen@gmail.com>
2019-09-22 09:34:16 -0400

    gqw fn && file

---
+++ src/lib/getQueriedWord.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const getQueriedWord = (text, hlText) => {
+
+    //remove existing tags associated with this selection
+    let removeRegex = /.\w*\s\w*=\"selected-text\".(\w*)<\/\w*>/g;
+    
+    /*
+        let newStyleRegex = new RegExp(`\W(${hlText})\W`, 'gi');
+        IN JS th '\' has to be escaped, a-la \\b 
+    */ 
+    
+    // add tags associated with this selection
+    let newStyleRegex = new RegExp(`\\b(${hlText})\\b`, 'gi');
+    
+    //update the text body
+    let resText = text.replace(removeRegex,"$1").replace(newStyleRegex, `<span class="selected-text">$1</span>`)
+
+    
+    return <p dangerouslySetInnerHTML={{__html: resText}}></p>
+
+}
+
+export {getQueriedWord}
