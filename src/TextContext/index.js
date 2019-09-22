f6dc8b84ec0ef01d903b5958cbdd4c2e32d34a3d
Eric Laursen <ericjlaursen@gmail.com>
2019-09-22 09:37:38 -0400

    code-cleanup

--- src/TextContext/index.js
+++ src/TextContext/index.js
@@ -1,27 +1,7 @@
 import React from 'react';
 let TextContext = React.createContext(); 
 let {Provider, Consumer} = TextContext
-
-const getQueriedWord = (text, hlText) => {
-
-    //remove existing tags associated with this selection
-    let removeRegex = /.\w*\s\w*=\"selected-text\".(\w*)<\/\w*>/g;
-    
-    /*
-        let newStyleRegex = new RegExp(`\W(${hlText})\W`, 'gi');
-        IN JS th '\' has to be escaped, a-la \\b 
-    */ 
-    
-    // add tags associated with this selection
-    let newStyleRegex = new RegExp(`\\b(${hlText})\\b`, 'gi');
-    
-    //update the text body
-    let resText = text.replace(removeRegex,"$1").replace(newStyleRegex, `<span class="selected-text">$1</span>`)
-
-    
-    return <p dangerouslySetInnerHTML={{__html: resText}}></p>
-
-}
+import {getQueriedWord} from '../lib/getQueriedWord'
 
 let TextProvider = (props) => {
 
@@ -31,42 +11,29 @@ let TextProvider = (props) => {
 	const initialContext = {text: ''};
 
 	const reducer = (state, action) => {
-		console.log('TextProvider reducer switch!!');
 			let resText;
 		switch(action.type){
-			case "COMMON_WORD":
-				/*
-					WHERE will the text-blob property, here, come from?
-
-				*/
-				resText = getQueriedWord(text, action.payload)
-				return {text: resText}
 			default:
-				resText = getQueriedWord(action.payload)
-				return {text: resText}	
+				return {...state, text: action.payload}	
 		}
 	}
 
-	let [commonWord, setCommonWord] = React.useState(null)
-	let [{text}, textDispatch] = React.useReducer(reducer, initialContext)
+	let [textStore, textDispatch] = React.useReducer(reducer, initialContext)
 
-	//load the text from textFile on load
+	/*
+		load the text from textFile 'on load'
+	*/
 	React.useEffect(() => {
 		console.log('LOADING TEXT in textProvider');
 		fetch('../../data/demo.txt')
 			.then(res => res.text().then(textRes => {
+				console.log('Fetched Data');
 					textDispatch({type: "TEXT", payload: textRes})
 				}))
 	}, [])
 
-	React.useEffect(() => {
-		if(commonWord !== null){
-			console.log('New Common word in text provider!!');
-		}
-	}, [commonWord])
-
-	//build provider value
-	let providerVal = {text, textDispatch}
+	//build provider value, CONST
+	const providerVal = {textStore, textDispatch}
 	
 	return(<Provider value={providerVal}>
 		{props.children}
