74580b19a2b2a7ed0a732ef44a84cef7a808cbb9
Eric Laursen <ericjlaursen@gmail.com>
2019-09-25 05:43:20 -0400

    imports update TextDisplat

--- src/routes/CombineTwo/index.js
+++ src/routes/CombineTwo/index.js
@@ -4,7 +4,7 @@ import * as d3Arr from 'd3-array'
 import * as d3Shape from 'd3-shape'
 import * as d3Select from 'd3-selection'
 import * as brush from 'd3-brush'
-import TextDisplay from '../../components/TextDisplay/OnlyTextWithContext'
+import TextDisplay from '../../components/TextDisplay/UsingComplexContext'
 import SelectableArea from '../../components/SelectableArea/UsingContext'
 // import './index.css'
 
