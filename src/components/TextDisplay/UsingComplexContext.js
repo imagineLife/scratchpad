ca87fbcc4bdbab318c87c3b3009f20875e2502e3
Eric Laursen <ericjlaursen@gmail.com>
2019-09-26 05:50:58 -0400

    uses TextArea context

--- src/components/TextDisplay/UsingComplexContext.js
+++ src/components/TextDisplay/UsingComplexContext.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './index.css'
 
-import { TextContext } from '../../TextContext/complex'
+import { TextAreaContext } from '../../Contexts/TextArea'
 import { getQueriedWord } from '../../lib/getQueriedWord'
 
 const TextDisplay = React.memo(function TextDisplay(){
 	console.log('%c TextDisplay Render', 'background-color: orange; color: white;')
 	
     // const {textStore, textDispatch} = React.useContext(TextContext);
-    const { textStore } = React.useContext(TextContext);
+    const { textStore } = React.useContext(TextAreaContext);
     console.log('{ textStore }')
     console.log(textStore)
 
