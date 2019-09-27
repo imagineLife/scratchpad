b72364a9e79304ef8bde9a588799f12c62db0fa6
Eric Laursen <ericjlaursen@gmail.com>
2019-09-27 06:28:40 -0400

    more cases, more state

--- src/Contexts/TextArea/index.js
+++ src/Contexts/TextArea/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 let TextAreaContext = React.createContext(); 
-let {Provider, Consumer} = TextAreaContext
+let {Provider, Consumer} = TextAreaContext;
 import {getQueriedWord} from '../../lib/getQueriedWord'
-import { getSentences } from '../../lib/stats'
+import { getSentences, getWordsByCount } from '../../lib/stats'
+
+const updateDisplayText = (sentenceArr, selectedSentenceArr) => {
+	let resString = '';
+	let curIdx = selectedSentenceArr[0];
+	while(curIdx < selectedSentenceArr[1]){
+		resString += sentenceArr[curIdx++].text+' '
+	}
+	return resString
+}
 
 let TextAreaProvider = (props) => {
 	/*
@@ -10,19 +19,48 @@ let TextAreaProvider = (props) => {
 	*/
 	const initialContext = {text: ''};
 
-	const reducer = (state, action) => {
-			let resText;
-		switch(action.type){
-			case "SENTENCES": {
-				return {...state, sentences: action.payload}
+	/*
+		STATE
+			{
+				text: 'string...',
+				displayText: 'stri..' (affected by area-selection)
+				sentences: [
+					{y: 8}
+				]
 			}
+	*/
+	const reducer = (state, action) => {
+		let resText;
+			
+		switch(action.type){
+			case "SENTENCES": 
+				return {
+					...state, 
+					sentences: action.payload
+				}
+				break;
+			case "UPDATE_DISPLAY_TEXT_FROM_AREA":
+				return {
+					...state,
+					displayText: updateDisplayText(state.sentences, action.payload)	// [0, 23]
+				}
+				break;
+			case "COMMON_WORDS": 
+				return {
+					...state,
+					commonWords: action.payload
+				}
+				break;
 			default:
 				return {...state, text: action.payload}	
+				break;
 		}
 	}
 
-	let [textStore, textDispatch] = React.useReducer(reducer, initialContext)
+
+	let [textStore, textAreaDispatch] = React.useReducer(reducer, initialContext)
 	let [areaData, setAreaData] = React.useState(null)
+
 	/*
 		load the text from textFile 'on load'
 	*/
@@ -31,13 +69,38 @@ let TextAreaProvider = (props) => {
 		fetch('../../data/fullText.txt')
 			.then(res => res.text().then(textRes => {
 				console.log('Fetched Data');
-					textDispatch({type: "TEXT", payload: textRes})
-					textDispatch({type: "SENTENCES", payload: getSentences(textRes)})
+
+					/*
+						regex Prep 
+						taken from say-what-api
+						for most-common word, 
+							&& other word-statistics
+					*/
+
+					//gets rid of line-break or whatever
+		      let newReg = /(^)?\s*$/gm;
+
+		      let uniqueWordRegex = /([a-z]\w+)(?![\s\S]*\1)/gi
+
+		      //remove some punc
+		      let puncRegEx = /[.,-]/g
+
+		      //apply regex
+		      const regexTxt = textRes.replace(newReg," ").replace(puncRegEx, "")
+		      const uniqueWordCount = textRes.match(uniqueWordRegex).length
+		      
+		      //split txt into arr of words
+		      let arrOfText =  regexTxt.split(" ")
+
+		      //update Provider state, triggering reducer with dispatched actions
+					textAreaDispatch({type: "TEXT", payload: textRes})
+					textAreaDispatch({type: "SENTENCES", payload: getSentences(textRes)})
+					textAreaDispatch({type: "COMMON_WORDS", payload: getWordsByCount(arrOfText).slice(0,9)})
 				}))
 	}, [])
 
 	//build provider value, CONST
-	const providerVal = {textStore, textDispatch, areaData, setAreaData}
+	const providerVal = {textStore, textAreaDispatch, areaData, setAreaData}
 	
 	return(<Provider value={providerVal}>
 		{props.children}
