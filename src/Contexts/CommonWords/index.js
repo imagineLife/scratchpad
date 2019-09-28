bd23a2d594cc22ad86d776dfe285e295c299d4ec
Eric Laursen <ericjlaursen@gmail.com>
2019-09-28 16:00:18 -0400

    includes new makeCommonWord Fn

--- src/Contexts/CommonWords/index.js
+++ src/Contexts/CommonWords/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
 const WordListContext = React.createContext(); 
 const {Provider, Consumer} = WordListContext;
-// import { TextContext } from '../TextContext'
+import { getWordsByCount } from '../../lib/stats'
+// import { TextAreaProvider } from '../TextArea/'
 
 const CommonWordsProvider = (props) => {
 
+	// let tpvals = React.useContext(TextAreaProvider)	
 	let [selectedWord, setSelectedWord] = React.useState(null);
-	let [commonWords] = React.useState(['president', 'the', 'we', 'to'])
+	let [commonWords, setCommonWords] = React.useState([])
 
-	let contextVal = {
-		selectedWord:selectedWord,
-		setSelectedWord,
-		commonWords:commonWords
-	}	
-	
-	return(<Provider value={contextVal}>
+	const makeCommonWords = (sentenceArr)=> {
+		setCommonWords(getWordsByCount(sentenceArr).slice(0,9))
+	}
+	// let contextVal = { selectedWord, setSelectedWord }	
+
+// console.log('%c C o m o n words context', 'background-color: pink; color: white;')
+	return(<Provider value={{ selectedWord, setSelectedWord, makeCommonWords }}>
 		{props.children}
 	</Provider>)
 }
