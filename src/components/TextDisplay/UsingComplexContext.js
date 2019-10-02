672b7db2be88921b89bd3ca92a41b05731963e28
Eric Laursen <ericjlaursen@gmail.com>
2019-10-02 05:38:40 -0400

    runs fn on selectedWord

--- src/components/TextDisplay/UsingComplexContext.js
+++ src/components/TextDisplay/UsingComplexContext.js
@@ -2,17 +2,24 @@ import React from 'react';
 import './index.css'
 
 import { TextAreaContext } from '../../Contexts/TextArea'
+import { WordListContext } from '../../Contexts/CommonWords'
 import { getQueriedWord } from '../../lib/getQueriedWord'
 
 const TextDisplay = React.memo(function TextDisplay(){
   const { displayText } = React.useContext(TextAreaContext);
+  const { selectedWord } = React.useContext(WordListContext);
 
 	if(!displayText){
 		return(<p>Text Display Using Complex Context</p>)
 	}
 
-	return(
-		<p>{displayText}</p>)
+	let resText = <p>{displayText}</p>
+
+	if(selectedWord){
+		resText = getQueriedWord(displayText, selectedWord)
+	}
+
+	return(resText)
 })
 
 export default TextDisplay;
