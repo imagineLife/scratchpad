2f527dba741db3037ccd0d83dee04612864b0357
Eric Laursen <ericjlaursen@gmail.com>
2019-10-06 20:00:34 -0400

    Introduces reducer

--- src/Contexts/CommonWords/index.js
+++ src/Contexts/CommonWords/index.js
@@ -8,13 +8,26 @@ const CommonWordsProvider = (props) => {
  
  	const initialState = {
  		"Common Words": [],
- 		"Words By Length": [],
  		"Longest Words": []
  	};
 
  	const reducer = (state, action) => {
 		let resText;
 		switch(action.type){
+			case "LONGEST_WORD":
+				return {
+					...state,
+					"Longest Words" : action.payload
+				}
+				break;
+			
+			case "COMMON_WORDS":
+				return {
+					...state,
+					"Common Words" : action.payload
+				}
+				break;
+			
 			default:
 				return {...state}	
 				break;
@@ -22,22 +35,27 @@ const CommonWordsProvider = (props) => {
 	}
 
 
-	let [textStore, textAreaDispatch] = React.useReducer(reducer, initialState)
+	let [wordLists, setWordLists] = React.useReducer(reducer, initialState)
  
 	let txtVals = React.useContext(TextAreaContext)	
 
 	let [selectedWord, setSelectedWord] = React.useState(null);
 	let [commonWords, setCommonWords] = React.useState([])
 	let [longestNine, setLongestNine] = React.useState([])
-	// let [wordLists, setWordLists] = React.useState([])
 
 	const makeCommonWords = (sentencesString)=> {
 		let arrayOfWords = convertStrToWordArr(sentencesString)		
 		setCommonWords(getWordsByCount(arrayOfWords).slice(0,10))
 		setLongestNine(getLongestThirty(arrayOfWords).slice(0,10))
-		// setWordLists({"Common Words" : getWordsByCount(arrayOfWords).slice(0,10)})
-		// setWordLists({"Longest Words" : getLongestThirty(arrayOfWords).slice(0,10)})
+		setWordLists({type: "COMMON_WORDS", payload: getWordsByCount(arrayOfWords).slice(0,10)})
+		setWordLists({type: "LONGEST_WORD", payload: getLongestThirty(arrayOfWords).slice(0,10)})
 	}
+
+	console.log('%c ---wordLists---', 'background-color: steelblue; color: white;')
+	console.log(wordLists)
+	console.log('// - - - - - //')
+	
+	
 
 	return(<Provider value={{ selectedWord, setSelectedWord, makeCommonWords, commonWords, longestNine}}>
 		{props.children}
