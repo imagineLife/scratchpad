3a0374eef3b53eb6522b0d66ec8d7c4aae120a01
Eric Laursen <ericjlaursen@gmail.com>
2019-10-16 21:05:45 -0400

    one iteration of underlining theme

--- src/components/TextDisplay/UsingComplexContext.js
+++ src/components/TextDisplay/UsingComplexContext.js
@@ -6,20 +6,79 @@ import { WordListContext } from '../../Contexts/CommonWords'
 import { getQueriedWord } from '../../lib/getQueriedWord'
 
 const TextDisplay = React.memo(function TextDisplay(){
-  const { displayText } = React.useContext(TextAreaContext);
+
+	let selectedTheme = {
+		thankful : [1, 3]
+		// angry: [2]
+	}
+	//1. load txt string
+  const { displayText, selectedAreaArr } = React.useContext(TextAreaContext);
+
+  //2. get selected word from selectedWordList 
   const { selectedWord } = React.useContext(WordListContext);
+  
 
 	if(!displayText){
 		return(<p>Text Display Using Complex Context</p>)
 	}
 
-	let resText = <p>{displayText}</p>
+	//if no selected word && no 
+	let resText = displayText
 
+	//apply selected word to text display
 	if(selectedWord){
 		resText = getQueriedWord(displayText, selectedWord)
 	}
 
-	return(resText)
+	/* apply theme to theme-d sentence
+		
+		THEME => color
+		
+		sentence# => which part of text to underline
+
+		// INPUT
+		- text-blob => resText
+		- sentence array => 'sentences' (from textAreaContext)
+		- associate theme with sentence array (above example)
+		*/
+
+		if(selectedTheme){
+
+
+			
+			let firstSentenceIndex = selectedAreaArr[0]
+			console.log('firstSentenceIndex')
+			console.log(firstSentenceIndex)
+			
+			let twoWhiteSpaces = /(\s{2})/gm;
+		  let standarizeWS = /([?!.]\s)(.)/gm;  
+		  let sentRegex = /(([A-Z][a-z])|\s)+[^.!?]*([A-Za-z].[A-Za-z]|[^.!?].)/g;
+		  
+  		console.log('resText')
+  		console.log(resText)
+  		
+		  let inViewSentences = resText.replace(twoWhiteSpaces, " ")
+		    .replace(standarizeWS, ". $2")
+		    .match(sentRegex);
+
+		  	let curThemeStr = Object.keys(selectedTheme)
+		  	let curThemeArr = selectedTheme[curThemeStr]
+		  curThemeArr.forEach((theme, idx) => {
+		  		
+		  		//sentence offset magic
+		  		let sentenceOffset = theme - firstSentenceIndex
+		  		console.log('sentenceOffset')
+		  		console.log(sentenceOffset)
+		  		
+		  		//apply theme
+		  		inViewSentences[sentenceOffset] = `<span class="themed theme-${curThemeStr}">${inViewSentences[sentenceOffset]}</span>`
+		  })
+
+		  //re-join sentences
+		  resText = inViewSentences.join()
+
+		}
+		return <p className="display-text" dangerouslySetInnerHTML={{__html: resText}}></p>
 })
 
 export default TextDisplay;
