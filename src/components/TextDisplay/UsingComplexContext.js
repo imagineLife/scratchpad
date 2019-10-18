e3a9e03935ce6ad1f736980c5769c7405abfacfa
Eric Laursen <ericjlaursen@gmail.com>
2019-10-18 18:49:39 -0400

    updates theme-functionality

--- src/components/TextDisplay/UsingComplexContext.js
+++ src/components/TextDisplay/UsingComplexContext.js
@@ -8,15 +8,19 @@ import { getQueriedWord } from '../../lib/getQueriedWord'
 const TextDisplay = React.memo(function TextDisplay(){
 
 	let selectedTheme = {
-		thankful : [1, 3]
-		// angry: [2]
+		unity : {
+			we: [2],
+			together: [2,3]
+		}
 	}
+
 	//1. load txt string
-  const { displayText, selectedAreaArr } = React.useContext(TextAreaContext);
+  const { displayText, selectedAreaArr, sentences } = React.useContext(TextAreaContext);
 
   //2. get selected word from selectedWordList 
   const { selectedWord } = React.useContext(WordListContext);
-  
+
+  let displaySentences  = [];
 
 	if(!displayText){
 		return(<p>Text Display Using Complex Context</p>)
@@ -30,55 +34,57 @@ const TextDisplay = React.memo(function TextDisplay(){
 		resText = getQueriedWord(displayText, selectedWord)
 	}
 
-	/* apply theme to theme-d sentence
-		
-		THEME => color
-		
-		sentence# => which part of text to underline
+	/* apply theme to theme-d sentence*/
+	if(selectedTheme){
 
-		// INPUT
-		- text-blob => resText
-		- sentence array => 'sentences' (from textAreaContext)
-		- associate theme with sentence array (above example)
+		//first sentence index from text-area box
+		let firstSentenceIndex = selectedAreaArr[0]
+		//[0]
+		
+		/*TODO: 
+			Convert to use 'sentences' array from textAReaContext?! [ {text: 'sentenceOne...'...etc}]
+			&& area-selection array [0, 24] 
 		*/
+		let twoWhiteSpaces = /(\s{2})/gm;
+	  let standarizeWS = /([?!.]\s)(.)/gm;  
+	  let sentRegex = /(([A-Z][a-z])|\s)+[^.!?]*([A-Za-z].[A-Za-z]|[^.!?].)/g;
+		
+	  let inViewSentences = resText.replace(twoWhiteSpaces, " ")
+	    .replace(standarizeWS, ". $2")
+	    .match(sentRegex);
 
-		if(selectedTheme){
+	  /*
+			TODO:
+			Figure out best way to store & work with themes...
+	  */
+		let themesArray = Object.keys(selectedTheme) //['unity']
+		let curThemeKeyWords = selectedTheme[themesArray] // ['we', together]
 
+		//
+	  for(let k in curThemeKeyWords){ //k = we, k= together
+	  	
+	  	//Apply theme html to each sentence
+	  	//[2] or [2,3]
+		  curThemeKeyWords[k].forEach((sentenceNumber, idx) => {
 
-			
-			let firstSentenceIndex = selectedAreaArr[0]
-			console.log('firstSentenceIndex')
-			console.log(firstSentenceIndex)
-			
-			let twoWhiteSpaces = /(\s{2})/gm;
-		  let standarizeWS = /([?!.]\s)(.)/gm;  
-		  let sentRegex = /(([A-Z][a-z])|\s)+[^.!?]*([A-Za-z].[A-Za-z]|[^.!?].)/g;
-		  
-  		console.log('resText')
-  		console.log(resText)
-  		
-		  let inViewSentences = resText.replace(twoWhiteSpaces, " ")
-		    .replace(standarizeWS, ". $2")
-		    .match(sentRegex);
-
-		  	let curThemeStr = Object.keys(selectedTheme)
-		  	let curThemeArr = selectedTheme[curThemeStr]
-		  curThemeArr.forEach((theme, idx) => {
-		  		
 		  		//sentence offset magic
-		  		let sentenceOffset = theme - firstSentenceIndex
-		  		console.log('sentenceOffset')
-		  		console.log(sentenceOffset)
+		  		let sentenceOffset = sentenceNumber - firstSentenceIndex - 1 // deal with 0-based arr indexing
 		  		
+		  		const matchedThemeRegex = new RegExp(k, 'gi')
+		  		//italicize theme-key-word
+		  		let matchedThemeTxt = inViewSentences[sentenceOffset]
+		  			.replace(matchedThemeRegex, `<i>${k}</i>`) //figure-out the capitalization
+
 		  		//apply theme
-		  		inViewSentences[sentenceOffset] = `<span class="themed theme-${curThemeStr}">${inViewSentences[sentenceOffset]}</span>`
+		  		inViewSentences[sentenceOffset] = `<span class="themed theme-${k}">${matchedThemeTxt}</span>` //inViewSentences[sentenceOffset]
 		  })
+	  }
 
-		  //re-join sentences
-		  resText = inViewSentences.join()
+	  //re-join sentences
+	  resText = inViewSentences.join("")
 
-		}
-		return <p className="display-text" dangerouslySetInnerHTML={{__html: resText}}></p>
+	}
+	return <p className="display-text" dangerouslySetInnerHTML={{__html: resText}}></p>
 })
 
 export default TextDisplay;
