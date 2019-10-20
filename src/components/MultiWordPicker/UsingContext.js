b4d546737e525ada3d32ba8354f9a88d02247532
Eric Laursen <ericjlaursen@gmail.com>
2019-10-20 10:51:29 -0400

    clone of WordList

---
+++ src/components/MultiWordPicker/UsingContext.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './index.css'
+import { TextAreaContext } from '../../Contexts/TextArea'
+import { WordListContext } from '../../Contexts/CommonWords'
+import { getWordsByCount } from '../../lib/stats'
+
+
+const CommonWords = () => {
+	let {displayText} = React.useContext(TextAreaContext)
+	let { commonWords, makeCommonWords, selectedWord, setSelectedWord, longestNine } = React.useContext(WordListContext);
+	
+	React.useEffect(() => { //look into useLayoutEffect
+		if(displayText){
+			makeCommonWords(displayText)
+		}
+	}, [displayText])
+
+	if(!displayText && commonWords.length < 1){
+		return(<p>loading Common words...</p>)
+	}
+	
+	return(
+		<ul>
+			{commonWords.map((w,i) => (
+					<li 
+						key={`${w.word}${i}`} 
+						style={{
+							textDecoration: (selectedWord === w.word) ? 'underline' : 'none',
+							listStyleType: 'none',
+							cursor: 'pointer'
+						}}
+						className={'word'}
+						onClick={() => {
+							setSelectedWord(w.word)
+							// textDispatch({type:"COMMON_WORD",payload: w})
+						}}>
+						{w.word}
+					</li>
+				)
+			)}		
+		</ul>
+	)
+}
+
+export default CommonWords
