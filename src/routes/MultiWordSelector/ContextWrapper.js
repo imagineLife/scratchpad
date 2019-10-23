706cf5e8e29a06fe1ccd45002e00cfec21648083
Eric Laursen <ericjlaursen@gmail.com>
2019-10-23 06:23:16 -0400

    renames component

--- src/routes/MultiWordSelector/ContextWrapper.js
+++ src/routes/MultiWordSelector/ContextWrapper.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import CombineThree from './'
+import MultiWordSelector from './'
 
 import {TextAreaProvider} from '../../Contexts/TextArea'
 import {CommonWordsProvider} from '../../Contexts/CommonWords'
@@ -14,7 +14,7 @@ const Wrapped = () => {
 	return (
 		<TextAreaProvider>
 			<CommonWordsProvider>
-				<CombineThree />
+				<MultiWordSelector />
 			</CommonWordsProvider>
 		</TextAreaProvider>
 	)
