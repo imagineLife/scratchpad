61471d1a3c67eeea352f44acac7a450368f2d1fa
Eric Laursen <ericjlaursen@gmail.com>
2019-10-25 06:17:42 -0400

    Layout component

---
+++ src/routes/Layout/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './index.css'
+
+const Layout = () => {
+	return(
+		<main id="layout">
+			<div id="title-box" className="f-w i-b"></div>
+			<div id="area-box" className="i-b"></div>
+			<div id="focu-area-box" className="i-b"></div>
+			<div id="area-explain-box" className="i-b"></div>
+			<div id="theme-box" className="i-b"></div>
+			<div id="image-box" className="i-b"></div>
+			<div id="words-of-interest-box" className="i-b"></div>
+			<div id="words-by-length-box" className="i-b"></div>
+			<div id="divider-box" className="i-b"></div>
+			<div id="text-display-box" className="i-b"></div>
+		</main>
+	)
+}
+export default Layout
