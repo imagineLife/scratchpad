010209fcb13fd01ba0e7c3c13a29141349c9596f
Eric Laursen <ericjlaursen@gmail.com>
2019-10-26 14:04:23 -0400

    col- classes

--- src/routes/Layout/index.js
+++ src/routes/Layout/index.js
@@ -1,19 +1,40 @@
 import React from 'react'
+import './responsive.css'
 import './index.css'
 
 const Layout = () => {
 	return(
-		<main id="layout">
-			<div id="title-box" className="f-w i-b"></div>
-			<div id="area-box" className="i-b"></div>
-			<div id="focus-area-box" className="i-b"></div>
-			<div id="area-explain-box" className="i-b"></div>
-			<div id="theme-box" className="i-b"></div>
-			<div id="image-box" className="i-b"></div>
-			<div id="words-of-interest-box" className="i-b"></div>
-			<div id="words-by-length-box" className="i-b"></div>
-			<div id="divider-box" className="i-b"></div>
-			<div id="text-display-box" className="i-b"></div>
+		<main id="layout" className="layout">
+			<div id="title-box" className="f-w i-b col-12">
+				<p>Title Box</p>
+			</div>
+			<div id="area-box" className="i-b col-12">
+				<p>Area Box</p>
+			</div>
+			<div id="focus-area-box" className="i-b col-4">
+				<p>Focus-Area Box</p>
+			</div>
+			<div id="area-explain-box" className="i-b col-8">
+				<p>Area-Explain Box</p>
+			</div>
+			<div id="theme-box" className="i-b col-3">
+				<p>Theme Box</p>
+			</div>
+			<div id="image-box" className="i-b col-5">
+				<p>Image Box</p>
+			</div>
+			<div id="words-of-interest-box" className="i-b col-5">
+				<p>Words-Of-Interest Box</p>
+			</div>
+			<div id="words-by-length-box" className="i-b col-7">
+				<p>Bords-By-Length Box</p>
+			</div>
+			<div id="divider-box" className="i-b col-12">
+				<p>Divider Box</p>
+			</div>
+			<div id="text-display-box" className="i-b col-12">
+				<p>Text-Display Box</p>
+			</div>
 		</main>
 	)
 }
