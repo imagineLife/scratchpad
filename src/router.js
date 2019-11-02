cc5455e9c439c931d9265d33d45d4fc948e433c6
Eric Laursen <ericjlaursen@gmail.com>
2019-11-02 07:20:55 -0400

    new fourth route

--- src/router.js
+++ src/router.js
@@ -7,12 +7,48 @@ import WordLists from './routes/WordLists'
 import CombineContexted from './routes/Combine/ContextWrapper'
 import CombineTwoContexted from './routes/CombineTwo/ContextWrapper'
 import CombineThreeContexted from './routes/CombineThree/ContextWrapper'
+import CombineFourContexted from './routes/CombineFour/ContextWrapper'
 import MultiWordSelector from './routes/MultiWordSelector/ContextWrapper'
 import Circles from './routes/Circles'
 import Layout from './routes/Layout'
 import FlexLayout from './routes/FlexLayout'
 let Home = () => <div>Home</div>
 let Dash = () => <div>Dash</div>
+
+const mockCircleData = [
+  {
+    "size": 9,
+    "occurances": 16
+  },
+  {
+    "size": 8,
+    "occurances": 22
+  },
+  {
+    "size": 7,
+    "occurances": 32
+  },
+  {
+    "size": 6,
+    "occurances": 30
+  },
+  {
+    "size": 5,
+    "occurances": 39
+  },
+  {
+    "size": 4,
+    "occurances": 51
+  },
+  {
+    "size": 3,
+    "occurances": 90
+  },
+  {
+    "size": 2,
+    "occurances": 50
+  }
+]
 
 const ThisRouter = () => {
   return(
@@ -27,7 +63,8 @@ const ThisRouter = () => {
       <WordLists path="wordLists" />
       <CombineContexted path="combine"/>  
       <CombineTwoContexted path="combineTwo"/>  
-      <CombineThreeContexted path="combineThree"/>  
+      <CombineThreeContexted path="combineThree"/>
+      <CombineFourContexted path="combineFour"/>  
       <MultiWordSelector path="multiWord"/>  
       <Circles path="circles"/>  
       <Layout path="layout"/>  
