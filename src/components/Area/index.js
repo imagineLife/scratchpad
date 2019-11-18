41c638a4c87bb9cac0b9a1e745b0eb237c7ea337
Eric Laursen <ericjlaursen@gmail.com>
2019-11-17 19:05:06 -0500

    area chart

--- src/components/Area/index.js
+++ src/components/Area/index.js
@@ -1,22 +1,57 @@
 import React from 'react'
 import './index.css'
+import {TextAreaContext} from '../../Contexts/TextArea'
+import * as scale from'd3-scale';
+import * as d3Shape from'd3-shape';
+import * as arr from 'd3-array'
 
-const Area = () => {
+const Area = ({dims}) => {
+
+	let {sentences, selectedAreaArr, maxWordsPerSentence} = React.useContext(TextAreaContext)
+	
+	//sanity checking
+	if(!sentences || !selectedAreaArr || !dims){
+		return <p>loading area...</p>
+	}
+
+	let howManySentences = selectedAreaArr[1] - selectedAreaArr[0]
+	let xScale = scale.scaleLinear()
+	.domain([0, howManySentences])
+	.range([0, dims.width])
+
+	let selectedSentences = sentences.reduce((acc, curVal, curIdx) => {		
+		if (curIdx >= selectedAreaArr[0] && curIdx <= selectedAreaArr[1]){
+			return acc.concat(curVal)
+		}else{
+			return acc
+		}
+	}, [])
+
+	let yScale = scale.scaleLinear()
+	.domain([0, maxWordsPerSentence])//arr.max(selectedSentences, d => d.wordCount)])
+	.range([dims.height, 0])
+	
+	//build areaFn
+	let areaFn = d3Shape.area()
+		.x((d, i) => xScale(i + 1))
+		.y0(yScale(0))
+		.y1((d) => yScale(d.wordCount))
+		.curve(d3Shape.curveCatmullRom)
+
+	const pathD = areaFn(selectedSentences)
+
 	return(
 		<svg id="area" style={dims}>
 				<defs>
-			    <linearGradient id="myGradient" gradientTransform="rotate(90)">
-			      <stop offset="50%" stopColor="rgb(147,147,147)" />
-			      <stop offset="100%"  stopColor="rgba(49,54,61,0)" />
+			    <linearGradient id="areaGradient" gradientTransform="rotate(90)">
+			      <stop offset="1%" stopColor="rgb(147,147,147)" />
+			      <stop offset="95%"  stopColor="rgba(147,147,147,.05)" />
 			    </linearGradient>
 			  </defs>
-			  <g className="area-g-wrapper">
-			    
-			    {/*Area Path*/}
-			    <path 
-			      d={pathD}
-						fill={'url(#myGradient)'} />
-				</g>
+					{/*Area Path*/}
+		    <path 
+		      d={pathD}
+					fill={'rgba(147,147,147,.25)'} />
 		</svg>
 	)
 }
