f3d0bd68e1ba7de932d65222623c1e416bdcf068
Eric Laursen <ericjlaursen@gmail.com>
2019-11-20 06:12:18 -0500

    explanation-wrapper

--- src/components/Area/index.js
+++ src/components/Area/index.js
@@ -53,11 +53,13 @@ const Area = ({dims}) => {
       let sentenceNumber = Math.ceil(xScale.invert(xPos)) + selectedAreaArr[0]
       
       if(sentenceNumber > -1){
-      	console.log(sentences[sentenceNumber - 1])
         setCurSentence(sentences[sentenceNumber - 1].text)
       }
     } 
   }
+
+  const stoppedMoving = () => setCurSentence(null)
+
 	return(
 		<React.Fragment>
 			<svg 
@@ -65,7 +67,8 @@ const Area = ({dims}) => {
 				style={dims} 
 				className='area-svg'
 				onMouseOver={moused}
-	      onMouseMove={moused}>
+	      onMouseMove={moused}
+	      onMouseOut={stoppedMoving}>
 					<defs>
 				    <linearGradient id="areaGradient" gradientTransform="rotate(90)">
 				      <stop offset="1%" stopColor="rgb(147,147,147)" />
@@ -77,10 +80,15 @@ const Area = ({dims}) => {
 			      d={pathD}
 						fill={'rgba(147,147,147,.25)'} />
 			</svg>
-			<p className="explanatory-text">{!curSentence && `Hover over this area chart to highlight the sentence 
-									that was spoken at the specific point in time during the 
-									president’s address.  HOVER`}
-						{curSentence && curSentence}</p>
+			<div className="explanation-wrapper">
+				<p className="explanatory-text">
+					{!curSentence && `Hover over this area chart to highlight the sentence 
+						that was spoken at the specific point in time during the 
+						president’s address.  HOVER`
+					}
+					{curSentence && curSentence}
+				</p>
+			</div>
 		</React.Fragment>
 	)
 }
