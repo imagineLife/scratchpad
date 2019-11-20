c9a6cac2c8e14c53a9effa3bd2e0e4c92ddef154
Eric Laursen <ericjlaursen@gmail.com>
2019-11-19 19:59:34 -0500

    lazy loading routes

--- src/router.js
+++ src/router.js
@@ -1,21 +1,25 @@
 import React from "react"
 import { render } from "react-dom"
 import { Router, Link, Redirect } from "@reach/router"
-import SelectableArea from './routes/SelectableArea'
-import TextWithComplexState from './routes/TextWithComplexState'
-import WordLists from './routes/WordLists'
-import CombineContexted from './routes/Combine/ContextWrapper'
-import CombineTwoContexted from './routes/CombineTwo/ContextWrapper'
-import CombineThreeContexted from './routes/CombineThree/ContextWrapper'
-import CombineFourContexted from './routes/CombineFour/ContextWrapper'
-import CombineFiveContexted from './routes/CombineFive/ContextWrapper'
-import MultiWordSelector from './routes/MultiWordSelector/ContextWrapper'
-import Circles from './routes/Circles'
-import Layout from './routes/Layout'
-import FlexLayout from './routes/FlexLayout'
-import FlexGrid from './routes/FlexGrid'
-import Together from './routes/Together/ContextWrapper'
 import './index.css'
+// import FlexGrid from './routes/FlexGrid';
+// import FlexLayout from './routes/FlexLayout';
+// import Layout from './routes/Layout';
+// import Circles from './routes/Circles';
+const SelectableArea = React.lazy(() =>  import('./routes/SelectableArea'))
+const TextWithComplexState = React.lazy(() =>  import('./routes/TextWithComplexState'))
+const WordLists = React.lazy(() =>  import('./routes/WordLists'))
+const CombineContexted = React.lazy(() =>  import('./routes/Combine/ContextWrapper'))
+const CombineTwoContexted = React.lazy(() =>  import('./routes/CombineTwo/ContextWrapper'))
+const CombineThreeContexted = React.lazy(() =>  import('./routes/CombineThree/ContextWrapper'))
+const CombineFourContexted = React.lazy(() =>  import('./routes/CombineFour/ContextWrapper'))
+const CombineFiveContexted = React.lazy(() =>  import('./routes/CombineFive/ContextWrapper'))
+const MultiWordSelector = React.lazy(() =>  import('./routes/MultiWordSelector/ContextWrapper'))
+const Circles = React.lazy(() =>  import('./routes/Circles'))
+const Layout = React.lazy(() =>  import('./routes/Layout'))
+const FlexLayout = React.lazy(() =>  import('./routes/FlexLayout'))
+const FlexGrid = React.lazy(() =>  import('./routes/FlexGrid'))
+const Together = React.lazy(() =>  import('./routes/Together/ContextWrapper'))
 
 let Home = () => <div>Home</div>
 let Dash = () => <div>Dash</div>
@@ -24,30 +28,32 @@ const ThisRouter = () => {
   let [themeClass, setThemeClass] = React.useState('dark'); //light
 
   return(
-    <div id="parent-div" className={`${themeClass}`}>
-      <Router>
-        <Home path="/" />
-        <Dash path="dashboard" />
-        <SelectableArea path="selectable" areaVars={{
-        	w: 700,
-        	h: 100
-        }}/>
-        <TextWithComplexState path="complexText"/>  
-        <WordLists path="wordLists" />
-        <CombineContexted path="combine"/>  
-        <CombineTwoContexted path="combineTwo"/>  
-        <CombineThreeContexted path="combineThree"/>
-        <CombineFourContexted path="combineFour"/>  
-        <CombineFiveContexted path="combineFive"/>  
-        <MultiWordSelector path="multiWord"/>  
-        <Circles path="circles"/>  
-        <Layout path="layout"/>  
-        <FlexLayout path="flex"/>  
-        <FlexGrid path="flexgrid"/>
-        <Together path="together"/>  
-        <Redirect from="/*" to="/"/>
-      </Router>
-    </div>
+    <React.Suspense fallback={<div>loading...</div>}>
+        <div id="parent-div" className={`${themeClass}`}>
+          <Router>
+            <Home path="/" />
+            <Dash path="dashboard" />
+            <SelectableArea path="selectable" areaVars={{
+            	w: 700,
+            	h: 100
+            }}/>
+            <TextWithComplexState path="complexText"/>  
+            <WordLists path="wordLists" />
+            <CombineContexted path="combine"/>  
+            <CombineTwoContexted path="combineTwo"/>  
+            <CombineThreeContexted path="combineThree"/>
+            <CombineFourContexted path="combineFour"/>  
+            <CombineFiveContexted path="combineFive"/>  
+            <MultiWordSelector path="multiWord"/>  
+            <Circles path="circles"/>  
+            <Layout path="layout"/>  
+            <FlexLayout path="flex"/>  
+            <FlexGrid path="flexgrid"/>
+            <Together path="together"/>  
+            <Redirect from="/*" to="/"/>
+          </Router>
+        </div>
+    </React.Suspense>
   )
 } 
 
