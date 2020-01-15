e5d755b21414b004caa04c46ea3bb6c35671f21f
Eric Laursen <ericjlaursen@gmail.com>
2020-01-15 06:34:03 -0500

    only-one-route

--- src/router.js
+++ src/router.js
@@ -2,27 +2,7 @@ import React from "react"
 import { render } from "react-dom"
 import { Router, Link, Redirect } from "@reach/router"
 import './index.css'
-// import FlexGrid from './routes/FlexGrid';
-// import FlexLayout from './routes/FlexLayout';
-// import Layout from './routes/Layout';
-// import Circles from './routes/Circles';
-const SelectableArea = React.lazy(() =>  import('./routes/SelectableArea'))
-const TextWithComplexState = React.lazy(() =>  import('./routes/TextWithComplexState'))
-const WordLists = React.lazy(() =>  import('./routes/WordLists'))
-const CombineContexted = React.lazy(() =>  import('./routes/Combine/ContextWrapper'))
-const CombineTwoContexted = React.lazy(() =>  import('./routes/CombineTwo/ContextWrapper'))
-const CombineThreeContexted = React.lazy(() =>  import('./routes/CombineThree/ContextWrapper'))
-const CombineFourContexted = React.lazy(() =>  import('./routes/CombineFour/ContextWrapper'))
-const CombineFiveContexted = React.lazy(() =>  import('./routes/CombineFive/ContextWrapper'))
-const MultiWordSelector = React.lazy(() =>  import('./routes/MultiWordSelector/ContextWrapper'))
-const Circles = React.lazy(() =>  import('./routes/Circles'))
-const Layout = React.lazy(() =>  import('./routes/Layout'))
-const FlexLayout = React.lazy(() =>  import('./routes/FlexLayout'))
-const FlexGrid = React.lazy(() =>  import('./routes/FlexGrid'))
 const Together = React.lazy(() =>  import('./routes/Together/ContextWrapper'))
-
-let Home = () => <div>Home</div>
-let Dash = () => <div>Dash</div>
 
 const ThisRouter = () => {
   let [themeClass, setThemeClass] = React.useState('dark'); //light
@@ -31,26 +11,8 @@ const ThisRouter = () => {
     <React.Suspense fallback={<div>loading...</div>}>
         <div id="parent-div" className={`${themeClass}`}>
           <Router>
-            <Home path="/" />
-            <Dash path="dashboard" />
-            <SelectableArea path="selectable" areaVars={{
-            	w: 700,
-            	h: 100
-            }}/>
-            <TextWithComplexState path="complexText"/>  
-            <WordLists path="wordLists" />
-            <CombineContexted path="combine"/>  
-            <CombineTwoContexted path="combineTwo"/>  
-            <CombineThreeContexted path="combineThree"/>
-            <CombineFourContexted path="combineFour"/>  
-            <CombineFiveContexted path="combineFive"/>  
-            <MultiWordSelector path="multiWord"/>  
-            <Circles path="circles"/>  
-            <Layout path="layout"/>  
-            <FlexLayout path="flex"/>  
-            <FlexGrid path="flexgrid"/>
-            <Together path="together"/>  
-            <Redirect from="/*" to="/"/>
+            <Together path="/"/>  
+            <Redirect from="/*" to="/" noThrow/>
           </Router>
         </div>
     </React.Suspense>
