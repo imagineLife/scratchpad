e6828be6ce57d45adbd14662bf60dd454bbf20d3
Eric Laursen <ericjlaursen@gmail.com>
2020-02-03 06:02:29 -0500

    i instead of span, linted

--- src/lib/getQueriedWord.js
+++ src/lib/getQueriedWord.js
@@ -1,23 +1,21 @@
 import React from 'react';
 
-    //(text: source text blob, hlText: selectedWord-to-hightlight)
+// (text: source text blob, hlText: selectedWord-to-hightlight)
 const getQueriedWord = (text, hlText, classToUse) => {
+  // remove any pre-existing classToUse where the selected-text goes
+  const removeRegex = new RegExp(`/.\w*\s\w*=\/${classToUse}.(\w*)<\/\w*>/g`);
 
-    //insert classToUse in where the selected-text goes
-    let removeRegex = new RegExp("/.\w*\s\w*=\/" + classToUse + ".(\w*)<\/\w*>/g")
-    
-    // add tags associated with this selection
-    // let newStyleRegex = new RegExp(`\\b(${hlText})\\b`, 'gi');
-    let newStyleRegex = new RegExp("\\b(" + hlText + ")\\b", 'gi');
-    
-    //update the text body
-    // let resText = text.replace(removeRegex,"$1").replace(newStyleRegex, `<span class="selected-text">$1</span>`)
-    let resText = text
-        .replace(removeRegex,"$1")
-        .replace(newStyleRegex, `<span class="${classToUse}">$1</span>`)
-    
-    return resText
+  // add tags associated with this selection
+  // let newStyleRegex = new RegExp(`\\b(${hlText})\\b`, 'gi');
+  const newStyleRegex = new RegExp(`\\b(${hlText})\\b`, 'gi');
 
-}
+  // update the text body
+  // let resText = text.replace(removeRegex,"$1").replace(newStyleRegex, `<span class="selected-text">$1</span>`)
+  const resText = text
+    .replace(removeRegex, '$1')
+    .replace(newStyleRegex, `<i class="${classToUse}">$1</i>`);
 
-export {getQueriedWord}
+  return resText;
+};
+
+export { getQueriedWord };
