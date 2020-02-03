a1b12c345fa3a0eafaa625db5778fd726b575bcc
Eric Laursen <ericjlaursen@gmail.com>
2020-02-03 06:08:36 -0500

    linted, update comments

--- src/lib/getWordLength.js
+++ src/lib/getWordLength.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
-    //(text: source text blob, hlText: selectedWordLength-to-hightlight)
-const getWordLength = (text, textLength, classToUse) => { 
+// (text: source text blob, hlText: selectedWordLength-to-hightlight)
+const getWordLength = (text, textLength, classToUse) => {
+  const removeRegex = new RegExp(`/.\w*\s\w*=\/${classToUse}.(\w*)<\/\w*>/g`);
+  const newStyleRegex = new RegExp(`\\s(\\w{${textLength}})[\\s.!?,]`, 'g'); // "/\\s(\\w{"+textLength+"})[\\s.!?,]/"
 
-    let removeRegex = new RegExp("/.\w*\s\w*=\/" + classToUse + ".(\w*)<\/\w*>/g")
-    let newStyleRegex = new RegExp("\\s(\\w{"+textLength+"})[\\s.!?,]", "g"); //"/\\s(\\w{"+textLength+"})[\\s.!?,]/"
+  // update the text body
+  const resText = text
+    .replace(removeRegex, '$1')
+    .replace(newStyleRegex, ` <i class="${classToUse}">$1</i> `);
 
-    //update the text body
-    let resText = text
-        .replace(removeRegex,"$1")
-        .replace(newStyleRegex, ` <span class="${classToUse}">$1</span> `)
-    
-    return resText
+  return resText;
+};
 
-}
-
-export default getWordLength
+export default getWordLength;
