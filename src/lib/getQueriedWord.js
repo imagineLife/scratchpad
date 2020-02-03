e0b9880deba3136cc6acecf407476951720d1dd4
Eric Laursen <ericjlaursen@gmail.com>
2020-02-03 06:08:23 -0500

    updates comments

--- src/lib/getQueriedWord.js
+++ src/lib/getQueriedWord.js
@@ -5,12 +5,10 @@ const getQueriedWord = (text, hlText, classToUse) => {
   // remove any pre-existing classToUse where the selected-text goes
   const removeRegex = new RegExp(`/.\w*\s\w*=\/${classToUse}.(\w*)<\/\w*>/g`);
 
-  // add tags associated with this selection
-  // let newStyleRegex = new RegExp(`\\b(${hlText})\\b`, 'gi');
+  // add tags associated with this selection, funky \\ for js parsing
   const newStyleRegex = new RegExp(`\\b(${hlText})\\b`, 'gi');
 
   // update the text body
-  // let resText = text.replace(removeRegex,"$1").replace(newStyleRegex, `<span class="selected-text">$1</span>`)
   const resText = text
     .replace(removeRegex, '$1')
     .replace(newStyleRegex, `<i class="${classToUse}">$1</i>`);
