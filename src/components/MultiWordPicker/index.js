842958a7fd4eca99b976cc78c154ccb1d085b562
Eric Laursen <ericjlaursen@gmail.com>
2020-02-05 06:48:08 -0500

    migrates MiltuWordPicker to index file

--- src/components/MultiWordPicker/index.js
+++ src/components/MultiWordPicker/index.js
@@ -1,24 +1,88 @@
 import React from 'react';
-import './index.css'
+import './index.css';
+import { TextAreaContext } from '../../Contexts/TextArea';
+import { WordListContext } from '../../Contexts/CommonWords';
 
-const WordList = ({words, selectWord, selectedWord}) => {
+const CommonWords = () => {
+  const { displayText } = React.useContext(TextAreaContext);
+  const {
+    makeCommonWords,
+    wordLists,
+    selectWordList,
+    setSelectedWord,
+    selectedWord,
+    wordListFocus,
+  } = React.useContext(WordListContext);
 
-	return(<ul>
-		{words && words.map((w,i) => (
-			<li 
-				key={`${w}$i`} 
-				style={{
-					textDecoration: (selectedWord === w) ? 'underline' : 'none',
-					listStyleType: 'none',
-					cursor: 'pointer'
-				}}
-				className={'word'}
-				onClick={() => selectWord(w)}>
-				{w}
-			</li>
-		))}
-		{!words && <li>Empty Word List Here</li>}
-	</ul>)
-}
+  // MAKE common words after display-text gets added to context
+  React.useEffect(() => {
+    if (displayText) {
+      makeCommonWords(displayText);
+    }
+  }, [displayText]);
 
-export default WordList
+  // AUTO-SELECT a word-list 'onLoad'
+  React.useEffect(() => {
+    if (displayText && wordLists && !wordListFocus) {
+      const lists = Object.keys(wordLists);
+      selectWordList(lists[0]);
+    }
+  }, [displayText, wordListFocus, wordLists]);
+
+  if (!wordLists) {
+    return <p>Missing Word Lists</p>;
+  }
+
+  const lists = Object.keys(wordLists);
+
+  return (
+    <section id="words-of-interest-for-ui">
+      {/* List of List-Titles */}
+      <section id="word-list">
+        <h3 className="word-list-title">Word Lists</h3>
+        <ul id="word-lists">
+          {lists.map((l, idx) => (
+            <li
+              key={`${l}-${idx}`}
+              className={`word-list-option${wordListFocus === l ? ' selected' : ''}`}
+              onClick={() => {
+                selectWordList((l));
+              }}
+            >
+              {l}
+            </li>
+          ))}
+        </ul>
+      </section>
+
+      {/* Words from selected word-list */}
+      <section id="focus-words">
+        <h3 className="word-list-title">List Results</h3>
+        {wordListFocus && (
+          <ul id="focus-word-list">
+            {wordLists[wordListFocus].map((l, idx) => {
+            	const thisWord = wordListFocus === 'Common Words' ? l.word : l;
+					    	return (
+                <li
+    key={`${thisWord}-${idx}`}
+    className={`focus-word-option ${selectedWord && selectedWord === thisWord ? ' selected-focus-word' : ''}`}
+    onClick={() => {
+						          if (selectedWord === thisWord) {
+						            setSelectedWord(null);
+						          } else {
+						            setSelectedWord(thisWord);
+						          }
+						        }}
+  > 
+    {thisWord}
+  </li>
+						    );
+					    })}
+          </ul>
+        )}
+      </section>
+    </section>
+  );
+};
+
+export default CommonWords;
