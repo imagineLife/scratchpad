32dca3f92a74bea4876193a4c3196a52625856e7
Eric Laursen <ericjlaursen@gmail.com>
2020-02-04 19:33:13 -0500

    updates newStyleRegex

--- src/lib/getWordLength.js
+++ src/lib/getWordLength.js
@@ -3,9 +3,9 @@ import React from 'react';
 // (text: source text blob, hlText: selectedWordLength-to-hightlight)
 const getWordLength = (text, textLength, classToUse) => {
   const removeRegex = new RegExp(`/.\w*\s\w*=\/${classToUse}.(\w*)<\/\w*>/g`);
-  const newStyleRegex = new RegExp(`\\s(\\w{${textLength}})[\\s.!?,]`, 'g'); // "/\\s(\\w{"+textLength+"})[\\s.!?,]/"
 
-  // update the text body
+  const newStyleRegex = new RegExp(`\\s(\\w{${textLength}})[\\s.!?,]`, 'g');
+
   const resText = text
     .replace(removeRegex, '$1')
     .replace(newStyleRegex, ` <i class="${classToUse}">$1</i> `);
