46e3f4027abe7a7d0019f5179e50955c223ab53a
Eric Laursen <ericjlaursen@gmail.com>
2020-02-05 06:44:11 -0500

    migrates functioning version to here

--- src/components/SelectableArea/index.js
+++ src/components/SelectableArea/index.js
@@ -1,103 +1,143 @@
-import React from 'react'
-import * as scale from 'd3-scale'
-import * as d3Arr from 'd3-array'
-import * as d3Shape from 'd3-shape'
-import * as d3Select from 'd3-selection'
-import * as brush from 'd3-brush'
-import './index.css'
+import React, { Fragment } from 'react';
+import * as scale from 'd3-scale';
+import * as d3Arr from 'd3-array';
+import * as d3Shape from 'd3-shape';
+import * as d3Select from 'd3-selection';
+import * as brush from 'd3-brush';
+import './index.css';
 
-const SelectableArea = ({dims, onMove, data}) => {
-	let [brushFn, setBrushFn] = React.useState(null)
-	let [hoverArr, setHoverArr] = React.useState([0,175])
-	let [brushBox,setBrushBox] = React.useState(null)
-	let brushRef = React.useRef()
-	
-	function brushedFn(){
-		
-		var selectedPixels = d3Select.event.selection;
-		
-		let scaledBegin = Math.floor(translateScale(selectedPixels[0]))
-		let scaledEnd = Math.floor(translateScale(selectedPixels[1]))
+import { TextAreaContext } from '../../Contexts/TextArea';
 
-		setHoverArr(selectedPixels)
-		onMove([scaledBegin, scaledEnd])
-		
-	}
+const SelectableArea = ({ onMove, dims }) => {
+  if (!dims || !dims.height || !dims.width) {
+    return <p>...loading</p>;
+  }
 
-	/*
-		select && save the 'brushBox' to state
+  const {
+    sentences,
+    textAreaDispatch,
+    areaData,
+    setAreaData,
+  } = React.useContext(TextAreaContext);
+
+  const [brushFn, setBrushFn] = React.useState(null);
+  const [hoverArr, setHoverArr] = React.useState([0, 175]);
+  const [brushBoxG, setBrushBoxG] = React.useState(null);
+  const [brushed, setBrushed] = React.useState(false);
+  const [set, setSet] = React.useState(false);
+  const brushRef = React.useRef();
+
+  /*
+		called 'onBrush'
 	*/
-	React.useEffect(() => {
-		if(data && !brushBox){
-			setBrushBox(d3Select.select(brushRef.current))
-		}
-	}, [data])
-	
-	/*
-		connect the brush to the g wrapper?!
+  function brushedFn() {
+    const selectedPixels = d3Select.event.selection;
+    const scaledBegin = Math.floor(translateScale(selectedPixels[0]));
+    const scaledEnd = Math.floor(translateScale(selectedPixels[1]));
+    setHoverArr(selectedPixels);
+
+    // dispatch Context-updater
+    textAreaDispatch({ type: 'UPDATE_DISPLAY_TEXT_FROM_AREA', payload: [scaledBegin, scaledEnd] });
+
+    onMove([scaledBegin, scaledEnd]);
+  }
+
+  /*
+		Set Area Context data 'initially',
+			from textStore sentences array
 	*/
-	React.useEffect(() => {
-		if(brushRef.current && brushBox){
-			
-			//build the brushFn
-			brushFn = brush.brushX()
-				.handleSize(10)
-				.on('brush', brushedFn)
+  React.useEffect(() => {
+    if (sentences && !areaData) {
+      const preppedAreaData = [];
+      sentences.forEach((s) => {
+        preppedAreaData.push({ y: s.wordCount });
+      });
 
-			// set the brushFn to the burshBox, 'instantiating'
-			// the brush UI element(s)
-			brushBox.call(brushFn);
+      setAreaData(preppedAreaData);
+    }
+  }, [sentences]);
 
-			//set the initial overlay to 1/4 width
-			brushFn.move(brushBox, hoverArr)
-		}
-	}, [brushBox])
+  // //    //////////////////////// /////
+  //	connect the brush to the g wrapper
+  // //    //////////////////////// /////
+  React.useEffect(() => {
+    if (brushRef && brushRef.current && areaData) {
+      // build the brushFn
+      const thisBrushFN = brush.brushX()
+      // .extent([0,0], [[700, 100]])
+        .handleSize(10)
+        .on('brush', brushedFn);
 
+      // set the brushFn to the burshBox, 'instantiating'
+      // the brush UI element(s)
+      const thisBrushBox = d3Select.select(brushRef.current);
+      setTimeout(() => {
+        thisBrushBox.call(thisBrushFN);
 
-	/*
-		could add the useLayout Effect here to work-around d3 race issue(s)
-	*/
-	//////////////////////////// /////
-	//// default loading return /////
-	//////////////////////////// /////
-	if(!data){
-		return (<p>Loading data...</p>)
-	}
-	
+        // set the initial overlay to 1/4 width
+        thisBrushFN.move(thisBrushBox, hoverArr);
+      }, 10);
+    }
+  }, [brushRef, areaData]);
 
-	//////////////////////////// /////
-	//// 			If SourceData		   /////
-	//////////////////////////// /////
-	//Set Scales
-	let xScale = scale.scaleLinear()
-		.domain([0, data.length - 1])
-		.range([0, 700])
+  // ////////////////////////// /////
+  // // default loading return /////
+  // ////////////////////////// /////
+  if (!areaData || !sentences) {
+    return (<p>Loading areaData...</p>);
+  }
 
-	let yScale = scale.scaleLinear()
-		.domain([0, d3Arr.max(data, d => d.y)])
-		.range([100, 0])
+  // //////////////////////// /////
+  // 			If SourceData		   /////
+  // //////////////////////// /////
+  // Set Scales
+  const xScale = scale.scaleLinear()
+    .domain([0, areaData.length - 1])
+    .range([0, dims.width]);
 
-	let translateScale = scale.scaleLinear()
-		.domain([0, 700])
-		.range([0, data.length - 1])
+  const yScale = scale.scaleLinear()
+    .domain([0, d3Arr.max(areaData, (d) => d.y)])
+    .range([100, 0]);
 
-	//build areaFn
-	let areaFn = d3Shape.area()
-		.x((d, i) => xScale(i + 1))
-		.y0(100)
-		.y1((d) => yScale(d.y))
+  let translateScale = scale.scaleLinear()
+    .domain([0, dims.width])
+    .range([0, areaData.length - 1]);
 
-	const pathD = areaFn(data)
+  // build areaFn
+  const areaFn = d3Shape.area()
+    .x((d, i) => xScale(i + 1))
+    .y0(100)
+    .y1((d) => yScale(d.y))
+    .curve(d3Shape.curveCatmullRom);
 
-	return(
-		<svg id="selectable" style={dims}>
-		  <g className="g-wrapper">
-		    <path 
-		      d={pathD}
-					fill={'#ccc'} />
-				<g className="brush-g-window" ref={brushRef} />
-			</g>
-		</svg>)
-}
+  const pathD = areaFn(areaData);
 
-export default SelectableArea
+  if (!pathD || !xScale || !yScale || !areaFn || !translateScale) {
+    return (<p>loading...</p>);
+  }
+
+  return (
+    <svg id="selectable" style={dims}>
+      <defs>
+        <linearGradient id="myGradient" gradientTransform="rotate(90)">
+          <stop offset="50%" stopColor="rgb(147,147,147)" />
+          <stop offset="100%" stopColor="rgba(49,54,61,0)" />
+        </linearGradient>
+      </defs>
+      <g className="g-wrapper">
+
+        {/* Area Path */}
+        <path
+          d={pathD}
+          fill="url(#myGradient)"
+        />
+
+        {/* Brush Handle */}
+        <g className="brush-g-window" ref={brushRef} />
+
+      </g>
+    </svg>
+  );
+};
+
+export default SelectableArea;
