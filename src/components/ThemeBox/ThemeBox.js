06b140bdd0a79926dd8c22bf0d165867767854f5
Eric Laursen <ericjlaursen@gmail.com>
2020-02-20 05:40:40 -0500

    moves themeColors to TxtArea context

--- src/components/ThemeBox/ThemeBox.js
+++ src/components/ThemeBox/ThemeBox.js
@@ -5,23 +5,13 @@ import { TextAreaContext } from '../../Contexts/TextArea';
 
 const ThemeBox = () => {
   const {
-    selectedAreaArr, themesData, theme, textAreaDispatch,
+  	curColor,
+    selectedAreaArr,
+    textAreaDispatch,
+    themesData,
+    theme,
   } = useContext(TextAreaContext);
 
-  const themeColors = useState([
-  	'rgb(166,206,227)',
-  	'rgb(31,120,180)',
-  	'rgb(178,223,138)',
-  	'rgb(51,160,44)',
-  	'rgb(251,154,153)',
-  	'rgb(227,26,28)',
-  	'rgb(253,191,111)',
-  	'rgb(255,127,0)',
-  	'rgb(202,178,214)',
-  	'rgb(106,61,154)',
-  	'rgb(255,255,153)',
-  	'rgb(177,89,40)',
-  ]);
   /*
 		Get all selected Themes, store in a single Array, no duplicates
 	*/
@@ -69,7 +59,7 @@ const ThemeBox = () => {
 						    className="theme-li"
   >
 						    {<span className="theme-word">{thisTheme}</span>}
-						    {thisTheme == theme && <span className="selected-underline" />}
+						    {thisTheme == theme && <span style={{ backgroundColor: curColor }} className="selected-underline" />}
   </li>
 						))
           }
