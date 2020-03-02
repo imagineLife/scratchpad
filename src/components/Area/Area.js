9b9b843844f08f8e07ef895fac3bffdf89119cb5
Eric Laursen <ericjlaursen@gmail.com>
2020-03-02 06:26:19 -0500

    moves code

---
+++ src/components/Area/Area.js
@@ -0,0 +1,166 @@
+import React, { useState, useContext, Fragment } from 'react';
+import './index.css';
+import * as scale from 'd3-scale';
+import * as d3Shape from 'd3-shape';
+// import * as arr from 'd3-array';
+import { TextAreaContext } from '../../Contexts/TextArea';
+import { AreaContext } from './State/Context';
+
+const Area = () => {
+  const {
+    sentences,
+    selectedAreaArr,
+    maxWordsPerSentence,
+  } = useContext(TextAreaContext);
+
+  const { dims, hoverLine } = useContext(AreaContext);
+  const [curSentence, setCurSentence] = useState(null);
+  const [curSentenceObj, setCurSentenceObj] = useState(null);
+  const [sentenceNumber, setSentenceNumber] = useState(null);
+  const [xOffset, setXOffset] = useState(null);
+  const [showLine, setShowLine] = useState(null);
+  const [offestSentenceNumber, setOffsetSentenceNumber] = useState(null);
+
+  // sanity checking
+  if (!sentences || !selectedAreaArr || !dims) {
+    return <p>loading area...</p>;
+  }
+
+  const howManySentences = selectedAreaArr[1] - selectedAreaArr[0];
+  const xScale = scale.scaleLinear()
+    .domain([0, howManySentences])
+    .range([0, dims.width]);
+
+  const selectedSentences = sentences.reduce((acc, curVal, curIdx) => {
+    if (curIdx >= selectedAreaArr[0] && curIdx <= selectedAreaArr[1]) {
+      return acc.concat(curVal);
+    }
+    return acc;
+  }, []);
+
+  const yScale = scale.scaleLinear()
+    .domain([0, maxWordsPerSentence])// arr.max(selectedSentences, d => d.wordCount)])
+    .range([dims.height, 0]);
+
+  // build areaFn
+  const areaFn = d3Shape.area()
+    .x((d, i) => xScale(i))
+    .y0(yScale(0))
+    .y1((d) => yScale(d.wordCount));
+  // .curve(d3Shape.curveCatmullRom)
+
+  const pathD = areaFn(selectedSentences);
+
+  // mousedOver && mouseMove
+  const moused = (d) => {
+    const areaSVG = document.getElementsByClassName('area-svg')[0];
+
+    // $FlowSVGBug
+    const areaSVGXOffset = areaSVG.getBoundingClientRect().x;
+
+    const xPos = d.pageX - areaSVGXOffset;
+
+    if (xPos >= (xScale.range()[0] - 5)) {
+      const thisSentence = Math.ceil(xScale.invert(xPos)) + selectedAreaArr[0];
+      const sentenceWOffset = thisSentence - selectedAreaArr[0];
+      if (thisSentence > -1) {
+      	setCurSentenceObj(sentences[thisSentence]);
+        setCurSentence(sentences[thisSentence].text);
+        setSentenceNumber(thisSentence);
+        setOffsetSentenceNumber(sentenceWOffset);
+        setShowLine(true);
+        setXOffset(areaSVGXOffset);
+      }
+    }
+  };
+
+  const stoppedMoving = () => {
+  	setCurSentence(null);
+  	setSentenceNumber(null);
+    setOffsetSentenceNumber(null);
+  	setShowLine(false);
+  };
+
+  /*
+    Hover-line
+  */
+  const scaledX = xScale(offestSentenceNumber);
+
+  const optHoverLine = !hoverLine
+    || offestSentenceNumber < 0
+    || offestSentenceNumber > xScale.domain()[1]
+    || !showLine ? null : (
+      <line
+        pointerEvents="none"
+        strokeWidth="1"
+        stroke="rgb(150,150,150)"
+        strokeDasharray="5 15"
+        x1={scaledX}
+        x2={scaledX}
+        y1={yScale(0)}
+        y2={yScale(Math.max(...selectedSentences.map((d) => d.wordCount)) * 1.05)}
+      />
+    );
+
+  /*
+    Hover-circle
+  */
+  const hoverCircle = !hoverLine
+    || offestSentenceNumber < 0
+    || !showLine ? null : (
+      <circle
+        pointerEvents="none"
+        r={8}
+        fill="rgba(255,255,255,.3)"
+        stroke="white"
+        strokeWidth="1"
+        strokeDasharray="2 6"
+        cx={scaledX}
+        cy={yScale(selectedSentences[offestSentenceNumber].wordCount)}
+      />
+    );
+
+  return (
+    <>
+      <svg
+        id="area"
+        style={dims}
+        className="area-svg"
+        onMouseOver={moused}
+        onFocus={moused}
+        onMouseMove={moused}
+        onMouseOut={stoppedMoving}
+        onBlur={stoppedMoving}
+      >
+        <defs>
+          <linearGradient id="areaGradient" gradientTransform="rotate(90)">
+            <stop offset="1%" stopColor="rgb(147,147,147)" />
+            <stop offset="95%" stopColor="rgba(147,147,147,.05)" />
+          </linearGradient>
+        </defs>
+
+        {/* Area Path */}
+        <path
+          d={pathD}
+          fill="rgba(147,147,147,.25)"
+        />
+
+        {/* Optional hovered-line */}
+        {optHoverLine}
+
+        {/* Optional hovered-circle */}
+        {hoverCircle}
+      </svg>
+      <div className="explanation-wrapper">
+        <p className="explanatory-text">
+          {!curSentence && `Hover over this area chart to highlight the sentence 
+						that was spoken at the specific point in time during the 
+						president’s address.  HOVER`}
+          {curSentence && curSentence}
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Area;
