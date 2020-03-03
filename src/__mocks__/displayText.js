cf226cd56974d8b77af2aaba08a1988fc8e96481
Eric Laursen <ericjlaursen@gmail.com>
2020-03-03 06:18:24 -0500

    mock text

---
+++ src/__mocks__/displayText.js
@@ -0,0 +1,2 @@
+const txt = 'Chief Justice Roberts, President Carter, President Clinton, President Bush, President Obama, fellow Americans, and people of the world, thank you.';
+export default txt;
