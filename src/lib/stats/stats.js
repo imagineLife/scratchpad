50f67707fc76f259d79ee7cda4c1f0dc6764186d
Eric Laursen <ericjlaursen@gmail.com>
2021-10-10 19:34:09 -0400

    removes fn

--- src/lib/stats/stats.js
+++ src/lib/stats/stats.js
@@ -1,30 +1,5 @@
 const convertStrToWordArr = require('./convertStrToWordArr').default;
 
-function getWordsByLength(srcWordArr) {
-  const startingArr = (typeof srcWordArr === 'string') ? convertStrToWordArr(srcWordArr) : srcWordArr;
-
-  const wordsByLength = [];
-
-  startingArr.forEach((singleWord) => {
-    let thisIndex = null;
-    // check if this word is already in array
-    const thisWordLength = singleWord.length;
-    if (wordsByLength.some((arrObj, arrObjInd) => {
-      if (arrObj.size == thisWordLength) {
-        thisIndex = arrObjInd;
-      }
-      return arrObj.size == thisWordLength;
-    })
-    ) {
-      wordsByLength[thisIndex].occurances += 1;
-    } else {
-      wordsByLength.push({ size: thisWordLength, occurances: 1 });
-    }
-  });
-
-  return wordsByLength.sort((a, b) => b.occurances - a.occurances).slice(0, 8);
-}
-
 function getLongestThirty(arr) {
   const startingArr = (typeof arr === 'string') ? convertStrToWordArr(arr) : arr;
 
@@ -66,6 +41,5 @@ function getSentences(srcTxt) {
 
 module.exports = {
   getLongestThirty,
-  getSentences,
-  getWordsByLength
+  getSentences
 };
