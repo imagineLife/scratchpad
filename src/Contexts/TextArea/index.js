ec80650d7829dabdefbaab5639f187fb52f135eb
Eric Laursen <ericjlaursen@gmail.com>
2021-10-11 08:00:33 -0400

    moves themeColors to reducer

--- src/Contexts/TextArea/index.js
+++ src/Contexts/TextArea/index.js
@@ -10,20 +10,6 @@ const { Provider, Consumer } = TextAreaContext;
 
 const TextAreaProvider = (props) => {
   const initialContext = { text: '', curColor: null };
-  const [themeColors] = useState([
-    'rgb(166,206,227)',
-    'rgb(31,120,180)',
-    'rgb(178,223,138)',
-    'rgb(51,160,44)',
-    'rgb(251,154,153)',
-    'rgb(227,26,28)',
-    'rgb(253,191,111)',
-    'rgb(255,127,0)',
-    'rgb(202,178,214)',
-    'rgb(106,61,154)',
-    'rgb(255,255,153)',
-    'rgb(177,89,40)',
-  ]);
 
   const [textAnalyticsStore, textAreaDispatch] = useReducer(reducer, initialContext);
   const [areaData, setAreaData] = useState(null);
