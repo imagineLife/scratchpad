7efd4206eedacade59de13542c17e93b90d07939
Eric Laursen <ericjlaursen@gmail.com>
2021-10-11 07:03:23 -0400

    remove breaks

--- src/Contexts/CommonWords/reducer/index.js
+++ src/Contexts/CommonWords/reducer/index.js
@@ -5,25 +5,18 @@ const reducer = (state, {type, payload}) => {
       ...state,
       'Longest Words': payload,
     };
-    break;
-
   case 'COMMON_WORDS':
     return {
       ...state,
       'Common Words': payload,
     };
-    break;
-
   case 'ACTION_WORDS':
     return {
       ...state,
       'Action Words': payload,
     };
-    break;
-
   default:
     throw new Error(`CALLED COMMON-WORDS REDUCER WITH BAD TYPE: ${type}`);
-    break;
   }
 };
 module.exports = reducer;
