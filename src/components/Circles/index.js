5c8a65c44212c6020746457e51a22811258770f3
Eric Laursen <ericjlaursen@gmail.com>
2021-10-14 06:59:27 -0400

    format

--- src/components/Circles/index.js
+++ src/components/Circles/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 import './index.css';
 import * as s from 'd3-scale';
 import * as a from 'd3-array';
@@ -6,22 +6,45 @@ import useDimensions from '../../lib/useDims';
 import { TextAreaContext } from '../../Contexts/TextArea';
 import { CirclesContext } from '../../Contexts/Circles';
 
+function circlesReducer(state, {type, payload}){
+  switch(type){
+    case 'UPDATE_ON_RESIZE':
+      return {
+        ...state,
+        lessM: payload.lessM,
+        circleRadiusRange: payload.circleRadiusRange,
+        buffer: payload.buffer
+      }
+    default:
+      throw new Error(`Called CirclesReducer with incorrect type ${type}`)
+  }
+}
+
+const initialState = {
+  lessM : {},
+  circleRadiusRange: [],
+  buffer: 0
+}
+
 const makeDimsLessMargins = (height, width, m) => ({ h: height - m.t - m.b, w: width - m.l - m.r });
 const Circles = () => {
   const dims = useDimensions();
   const [ref, { height, width }] = dims;
-  const [m] = useState({
-    t: 10, r: 25, b: 5, l: 15,
-  });
-  const [lessM, setLessM] = useState({});
-  const [circleRadiusRange, setCircleRadiusRange] = useState([]);
-  const [buffer, setBuffer] = useState(0);
+  const m = { t: 10, r: 25, b: 5, l: 15 };
+  const [{
+    lessM,
+    circleRadiusRange,
+    buffer
+  }, dispatch] = useReducer(circlesReducer, initialState)
+
   const [hoveredCircle, setHoveredCircle] = useState(null);
 
   const updateFromResize = (passedDims, circleMax) => {
-    setLessM(passedDims);
-    setCircleRadiusRange([0, circleMax]);
-    setBuffer(passedDims.w * 0.01);
+    dispatch({type: 'UPDATE_ON_RESIZE', payload: {
+      lessM:passedDims,
+      circleRadiusRange : [0, circleMax],
+      buffer: passedDims.w * 0.01
+    }})
   };
 
   // load visible text string
@@ -97,47 +120,62 @@ const Circles = () => {
 						  const circleX = c.thisX + c.prevX;
 						  const circleY = lessM.h * 0.45;
 						  return (
-  <g
-  key={`${c.size}-${idx}`}
-  onClick={() => {
-										  if (wordLength === c.size) {
-										    textAreaDispatch({ type: 'WORD_LENGTH', payload: null });
-										  } else {
-										    textAreaDispatch({ type: 'WORD_LENGTH', payload: c.size });
-										  }
-						      }}
-						    >
-  <defs>
-  <filter id="glow">
-  <feGaussianBlur className="blur" stdDeviation="2.5" result="coloredBlur" />
-  <feMerge>
-  <feMergeNode in="coloredBlur" />
-  <feMergeNode in="SourceGraphic" />
-						          </feMerge>
-						        </filter>
-						      </defs>
-  <circle
-  className="word-circle"
-  r={rScale(c.occurances)}
-  stroke={wordLength == c.size ? 'rgb(125,125,0)' : hoveredCircle == idx ? 'rgb(85,85,30)' : 'rgb(125,125,125)'}
-  strokeWidth={2}
-  cx={circleX}
-  cy={circleY}
-  fill={wordLength == c.size ? 'rgb(25,25,0)' : 'rgb(25,25,25)'}
-  filter={hoveredCircle == idx ? 'url(#glow)' : null}
-  onMouseOver={() => { setHoveredCircle(idx); }}
-  onMouseOut={() => { setHoveredCircle(null); }}
-						      />
-  <text pointerEvents="none">
-  <tspan pointerEvents="none" x={circleX} y={lessM.h - 15} className="circle-label">
-  {c.size}
-  -Letter
-						        </tspan>
-  <tspan pointerEvents="none" x={circleX} y={lessM.h} className="circle-label">Words</tspan>
-						      </text>
-  <text pointerEvents="none">
-  <tspan pointerEvents="none" x={circleX} y={circleY} className="circle-label count">{c.occurances}</tspan>
-						      </text>
+                <g
+                  key={`${c.size}-${idx}`}
+                  onClick={() => {
+                    if (wordLength === c.size) {
+                      textAreaDispatch({ type: 'WORD_LENGTH', payload: null });
+                    } else {
+                      textAreaDispatch({ type: 'WORD_LENGTH', payload: c.size });
+                    }
+                  }}
+                >
+                  <defs>
+                  <filter id="glow">
+                  <feGaussianBlur className="blur" stdDeviation="2.5" result="coloredBlur" />
+                  <feMerge>
+                  <feMergeNode in="coloredBlur" />
+                  <feMergeNode in="SourceGraphic" />
+                                      </feMerge>
+                                    </filter>
+                                  </defs>
+                  <circle
+                    className="word-circle"
+                    r={rScale(c.occurances)}
+                    stroke={wordLength == c.size ? 'rgb(125,125,0)' : hoveredCircle == idx ? 'rgb(85,85,30)' : 'rgb(125,125,125)'}
+                    strokeWidth={2}
+                    cx={circleX}
+                    cy={circleY}
+                    fill={wordLength == c.size ? 'rgb(25,25,0)' : 'rgb(25,25,25)'}
+                    filter={hoveredCircle == idx ? 'url(#glow)' : null}
+                    onMouseOver={() => { setHoveredCircle(idx); }}
+                    onMouseOut={() => { setHoveredCircle(null); }}
+                  />
+                  <text pointerEvents="none">
+                    <tspan 
+                      pointerEvents="none" 
+                      x={circleX} 
+                      y={lessM.h - 15} 
+                      className="circle-label">
+                      {c.size}-Letter
+                    </tspan>
+                    <tspan 
+                      pointerEvents="none" 
+                      x={circleX} 
+                      y={lessM.h} 
+                      className="circle-label">
+                        Words
+                    </tspan>
+                  </text>
+                  <text pointerEvents="none">
+                    <tspan 
+                      pointerEvents="none" 
+                      x={circleX} 
+                      y={circleY} 
+                      className="circle-label count">
+                        {c.occurances}
+                    </tspan>
+                  </text>
 						    </g>
 						  );
 						})}
