01a65583f37f420aebd4b460ee322edde49910ba
Eric Laursen <ericjlaursen@gmail.com>
2021-11-10 07:57:19 -0500

    update test

--- src/index.test.js
+++ src/index.test.js
@@ -5,11 +5,11 @@ import Router from "./router";
 jest.mock("react-dom", () => ({ render: jest.fn() }));
 
 describe("Application root", () => {
-  it("should render without crashing", () => {
+  it("should not render when no doc", () => {
     const div = document.createElement("div");
     div.id = "root";
     document.body.appendChild(div);
     require("./index.js");
-    expect(ReactDOM.render).toHaveBeenCalledWith(<Router />, div);
+    expect(ReactDOM.render).toHaveBeenCalledWith(<Router />, null);
   });
 });
