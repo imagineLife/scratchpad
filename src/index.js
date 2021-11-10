291f1de40f51a5041fafe501caa6694235f1f2ee
Eric Laursen <ericjlaursen@gmail.com>
2021-11-10 07:45:35 -0500

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import pngImage from './static/say-what.png'
 import Router from './router'
-import './index.css'
-const App = () => {
-	return(<Router />)
-}
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<Router />, document.getElementById("app"));
