77937b9bf4112d3b063d8a8c19964508997db7c8
Eric Laursen <ericjlaursen@gmail.com>
2021-11-12 08:00:51 -0500

    mock props, mount with mock state

--- src/components/Area/index.test.js
+++ src/components/Area/index.test.js
@@ -1,11 +1,59 @@
 import React from 'react';
 import toJson from 'enzyme-to-json';
-import { shallow } from 'enzyme';
-import Area from '.';
+import { mount } from 'enzyme';
+import Area from './Area';
 
+// state
+import { TextAreaContext } from '../../Contexts/TextArea';
+import { AreaContext } from './State/Context';
+
+/*
+    const {
+    sentences,
+    selectedAreaArr,
+    maxWordsPerSentence
+  } = useContext(TextAreaContext);
+
+  const {
+    dims,
+    hoverLine,
+    curSentence,
+    moused,
+    offsetSentenceNumber,
+    showLine,
+    stoppedMoving
+  } = useContext(AreaContext);
+*/
 describe('<Area/>', () => {
+  const mousedFn = jest.fn()
+  const textAreaState = {
+    sentences: null,
+    selectedAreaArr: [],
+    maxWordsPerSentence: null
+  }
+
+  const areaState = {
+    dims: null,
+    hoverLine: null,
+    curSentence: null,
+    moused: mousedFn,
+    offsetSentenceNumber: null,
+    showLine: null,
+    stoppedMoving: null
+  }
+
+  const compNoProps = mount(
+    <TextAreaContext.Provider value={textAreaState}>
+      <AreaContext.Provider value={areaState}>
+        <Area />
+      </AreaContext.Provider>
+    </TextAreaContext.Provider>
+  );
   it('matches snapshot', () => {
-    const comp = shallow(<Area />);
-    expect(toJson(comp)).toMatchSnapshot();
+    expect(toJson(compNoProps)).toMatchSnapshot();
+  });
+
+  it('shows "loading area..." with no data', () => {
+    expect(compNoProps.find('p').text()).toBe('loading area...')
   });
 });
