6312946b467ba26f36de4f226fd6a04c72a5e2f8
Eric Laursen <ericjlaursen@gmail.com>
2021-11-16 06:56:36 -0500

    snap + val test

---
+++ src/components/Area/State/area-context.test.js
@@ -0,0 +1,42 @@
+import React, { useContext } from 'react';
+import { mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import { TextAreaContext, TextAreaProvider } from '../../../Contexts/TextArea';
+import { AreaContext, AreaProvider } from './Context';
+import mockTextAreaData from './mock-text-area-vals.json';
+import { waitForComponentToPaint } from '../../../helpers';
+
+describe('Area Context', () => {
+  const ChildBox = () => {
+    const {selectedAreaArr, ...ctxVals} = useContext(AreaContext)
+    console.log('ctxVals')
+    console.log(ctxVals)
+    
+    return <div id='child-box'>
+      <p id="selectedAreaArr">{JSON.stringify(selectedAreaArr)}</p>
+    </div>
+  }
+
+  const initialRender = mount(
+    <TextAreaContext.Provider value={{...mockTextAreaData}}>
+      <TextAreaContext.Consumer>
+      {(txtAreaCtx) => (
+        <AreaContext.Provider value={{...txtAreaCtx, dims: {width: 312.796875, height: 152}}}>
+          <ChildBox />
+        </AreaContext.Provider>
+      )}
+      </TextAreaContext.Consumer>
+    </TextAreaContext.Provider>
+  );
+  waitForComponentToPaint(initialRender)
+
+    it('matches snapshot', () => {
+      expect(toJson(initialRender)).toMatchSnapshot();
+    });
+  describe('shows content onLoad', () => {
+    it('selectedAreaArr', () => {
+      let arrVals = initialRender.find('p#selectedAreaArr').text()
+      expect(arrVals).toBe('[0,20]')
+    })
+  })
+})
