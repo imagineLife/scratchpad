ca462abb1d35c03c1fc862bb7a961dae7f111de1
Eric Laursen <ericjlaursen@gmail.com>
2021-11-18 07:17:30 -0500

    remove comments

--- webpack.config.js
+++ webpack.config.js
@@ -38,30 +38,9 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          /*
-            style-loader
-            adds the css script tag in the browers
-            consumes the css-loader arr
-          */
           'style-loader',
-          /*
-            css-loader TRANSFORMS the css to a js obj,
-            as seen through footerJS
-            import css from "./footer.css";
-            console.log('css')
-            console.log(css)
-
-            [
-              [
-                "./src/footer.css",
-                "footer {\n    height: 100px;\n    width: 100%;\n    background: yellowgreen;\n    border: 1px solid black;\n}",
-                ""
-              ]
-            ]
-          */
-          'css-loader',
-        ],
-        // use: [MiniCss.loader, "css-loader"]
+          'css-loader'
+        ]
       },
       {
         test: /\.(png|svg|jpe?g|gif|ico|txt)$/,
@@ -77,7 +56,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
-      filename: './index.html',
+      filename: './index.html'
     }),
     // shows webpack build process during build
     // as terminal output
